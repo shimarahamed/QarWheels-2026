82af8920fe53ee642c8b2d414f624d6e4053587d
Shimar Ahamed <shimarahamed2012@gmail.com>
2026-02-14 13:05:07 +0000

    Try fixing this error: `Runtime ReferenceError: CardFooter is not define

--- src/app/dashboard/garages/page.tsx
+++ src/app/dashboard/garages/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { mockGarages } from '@/lib/data';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
