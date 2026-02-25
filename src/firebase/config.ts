f91b371ffb51c8cd4116b0480185b2cde3c6129c
Shimar Ahamed <shimarahamed2012@gmail.com>
2026-02-25 07:07:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1664193926-a35e2",
+  "appId": "1:902599852979:web:bde4f5ce8b26c44181c1c6",
+  "apiKey": "AIzaSyAfMu2LTdvunXgLEpGrLWn-RY4-eEp1VT4",
+  "authDomain": "studio-1664193926-a35e2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "902599852979"
+};
