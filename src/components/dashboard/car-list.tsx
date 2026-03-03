908ddd2021c8b5fed2dc142b816214dc585ef179
Shimar Ahamed <shimarahamed2012@gmail.com>
2026-03-03 18:44:26 +0000

    Try fixing this error: `Runtime ReferenceError: Button is not defined. E

--- src/components/dashboard/car-list.tsx
+++ src/components/dashboard/car-list.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Car as CarIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 function CarCardSkeleton() {
   return (
